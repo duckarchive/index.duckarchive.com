49a3d6fad4b6bf9d545157794f5dded22eb63a4b
alexandrtovmach <alexandrtovmach@gmail.com>
2025-03-03 23:11:34 +0100

    feat: Changed home page to mockup

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,16 +38,16 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        {/* <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <Navbar /> */}
+        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+          {children}
+        </main>
+        {/* <footer className="w-full flex items-center justify-center py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
@@ -59,7 +59,7 @@ export default function RootLayout({
               </Link>
             </footer>
           </div>
-        </Providers>
+        </Providers> */}
       </body>
     </html>
   );
