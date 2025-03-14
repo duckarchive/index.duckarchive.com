2b212874b38cbc5b3ab6c68a44843a8f0e0a02d3
alexandrtovmach <alexandrtovmach@gmail.com>
2025-03-14 23:17:26 +0100

    feat: Improved first mapper parse logic

--- lib/mappers/gwar.mil.ru/chelovek_gospital.ts
+++ lib/mappers/gwar.mil.ru/chelovek_gospital.ts
@@ -1,3 +1,4 @@
+import { s } from "@/lib/parse";
 import { FileFormat, Mapper } from "@/types";
 
 export const config = {
@@ -12,15 +13,6 @@ export const config = {
 //   "_id": "23826076",
 //   "_score": 1,
 //   "_source": {
-//     "box_id": 6445,
-//     "event_place": null,
-//     "birth_date": null,
-//     "birth_place_gubernia": null,
-//     "document_author": null,
-//     "military_unit_id": 804,
-//     "lazaret_id": null,
-//     "__type": "chelovek_gospital",
-//     "rank": "рядовой",
 //     "documents_pages": {
 //       "documents_id": [
 //         23857238
@@ -29,82 +21,192 @@ export const config = {
 //         24349033
 //       ]
 //     },
-//     "id": 23826076,
-//     "birth_place_volost": null,
-//     "deal_id": null,
-//     "inventory_id": null,
-//     "document_date": null,
-//     "zahoronenie_volost": null,
-//     "birth_place": null,
-//     "archive": "Российский Государственный Военно-исторический архив",
-//     "zahoronenie_uezd": null,
-//     "shkaf_id": 1,
-//     "zahoronenie_mesto": null,
-//     "data_priziva": null,
-//     "birth_place_id": null,
-//     "inventory_num": null,
-//     "updated": "2018-11-02T00:00:00Z",
-//     "zahoronenie_gubernia": null,
-//     "vibitie_date_from": "1917-02-13",
-//     "vibitie_mesto": null,
-//     "birth_place_np_type": null,
-//     "deal": null,
-//     "person_type": "Списки потерь",
-//     "box": "1713-К",
-//     "fund_id": 2,
-//     "doc_type": "Карточка на прибывших (нижние чины)",
-//     "inventory": null,
-//     "shkaf_name": "без номера",
-//     "document_name": null,
-//     "operations": null,
-//     "birth_place_uezd": null,
-//     "document_num": null,
-//     "fund_num": null,
-//     "first_name": "Гаврил",
-//     "shkaf": "без номера",
-//     "birth_place_location": null,
-//     "archive_short": "РГВИА",
-//     "last_name": "Купряцкий",
-//     "middle_name": "Ефимович",
-//     "zahoronenie_location": null,
-//     "event_date_from": "1917-04-04",
-//     "zahoronenie_np_type": null,
-//     "event_date_to": "1917-04-04",
-//     "vibitie_date_to": "1917-02-13",
-//     "fund": "Картотека бюро учета потерь в Первой мировой войне (офицеров и солдат)",
-//     "military_unit_name": "405-й пехотный Льговский полк",
-//     "vibitie_prichina": "ранен/контужен",
+//     "__type": "chelovek_gospital",
 //     "archive_id": 1
+//     "archive_short": "РГВИА",
+//     "archive": "Российский Государственный Военно-исторический архив",
+//     "birth_date": null,
+//     "birth_place_gubernia": null,
+//     "birth_place_id": null,
+//     "birth_place_location": null,
+//     "birth_place_np_type": null,
+//     "birth_place_uezd": null,
+//     "birth_place_volost": null,
+//     "birth_place": null,
+//     "box_id": 6445,
+//     "box": "1713-К",
+//     "data_priziva": null,
+//     "deal_id": null,
+//     "deal": null,
+//     "doc_type": "Карточка на прибывших (нижние чины)",
+//     "document_author": null,
+//     "document_date": null,
+//     "document_name": null,
+//     "document_num": null,
+//     "event_date_from": "1917-04-04",
+//     "event_date_to": "1917-04-04",
+//     "event_place": null,
+//     "first_name": "Гаврил",
+//     "fund_id": 2,
+//     "fund_num": null,
+//     "fund": "Картотека бюро учета потерь в Первой мировой войне (офицеров и солдат)",
+//     "id": 23826076,
+//     "inventory_id": null,
+//     "inventory_num": null,
+//     "inventory": null,
+//     "last_name": "Купряцкий",
+//     "lazaret_id": null,
+//     "middle_name": "Ефимович",
+//     "military_unit_id": 804,
+//     "military_unit_name": "405-й пехотный Льговский полк",
+//     "operations": null,
+//     "person_type": "Списки потерь",
+//     "rank": "рядовой",
+//     "shkaf_id": 1,
+//     "shkaf_name": "без номера",
+//     "shkaf": "без номера",
+//     "updated": "2018-11-02T00:00:00Z",
+//     "vibitie_date_from": "1917-02-13",
+//     "vibitie_date_to": "1917-02-13",
+//     "vibitie_mesto": null,
+//     "vibitie_prichina": "ранен/контужен",
+//     "zahoronenie_gubernia": null,
+//     "zahoronenie_location": null,
+//     "zahoronenie_mesto": null,
+//     "zahoronenie_np_type": null,
+//     "zahoronenie_uezd": null,
+//     "zahoronenie_volost": null,
 //   }
 // }
+interface ContentItem {
+  _index: string;
+  _type: string;
+  _id: string;
+  _score: number;
+  _source: {
+    documents_pages: {
+      documents_id: number[];
+      pages_id: number[];
+    };
+    __type: string;
+    archive_id: number;
+    archive_short: string;
+    archive: string;
+    birth_date: string | null;
+    birth_place_gubernia: string | null;
+    birth_place_id: string | null;
+    birth_place_location: string | null;
+    birth_place_np_type: string | null;
+    birth_place_uezd: string | null;
+    birth_place_volost: string | null;
+    birth_place: string | null;
+    box_id: number;
+    box: string;
+    data_priziva: string | null;
+    deal_id: string | null;
+    deal: string | null;
+    doc_type: string;
+    document_author: string | null;
+    document_date: string | null;
+    document_name: string | null;
+    document_num: string | null;
+    event_date_from: string;
+    event_date_to: string;
+    event_place: string | null;
+    first_name: string;
+    fund_id: number;
+    fund_num: string | null;
+    fund: string;
+    id: number;
+    inventory_id: string | null;
+    inventory_num: string | null;
+    inventory: string | null;
+    last_name: string;
+    lazaret_id: string | null;
+    middle_name: string;
+    military_unit_id: number;
+    military_unit_name: string;
+    operations: string | null;
+    person_type: string;
+    rank: string;
+    shkaf_id: number;
+    shkaf_name: string;
+    shkaf: string;
+    updated: string;
+    vibitie_date_from: string;
+    vibitie_date_to: string;
+    vibitie_mesto: string | null;
+    vibitie_prichina: string;
+    zahoronenie_gubernia: string | null;
+    zahoronenie_location: string | null;
+    zahoronenie_mesto: string | null;
+    zahoronenie_np_type: string | null;
+    zahoronenie_uezd: string | null;
+    zahoronenie_volost: string | null;
+  };
+}
 
 export const parse: Mapper["parse"] = (content) =>
-  content.map(({ _source }) => ({
-    first_name: _source.first_name,
-    last_name: _source.last_name,
-    middle_name: _source.middle_name,
-    birth_date: _source.birth_date || "",
-    birth_place: [
-      _source.birth_place_gubernia,
-      _source.birth_place_uezd,
-      _source.birth_place_volost,
-      _source.birth_place_np_type,
-      _source.birth_place,
-    ]
-      .filter(Boolean)
-      .join(", "),
-    record_date: _source.event_date_from || "",
-    record_date_normalized: _source.event_date_from
-      ? new Date(_source.event_date_from)
-      : null,
-    record_place: _source.vibitie_mesto,
-    record_type: _source.doc_type,
-    archive: _source.archive_short,
-    fund: _source.fund_num,
-    description: _source.inventory_num || _source.box,
-    case: _source.document_num,
-    page: _source.documents_pages.pages_id.join(", "),
-    note: null,
-    author_id: "",
-    resource_id: "gwar.mil.ru",
-  }));
+  content.map((item) => {
+    const {
+      _source: {
+        archive_short,
+        birth_date,
+        birth_place_gubernia,
+        birth_place_np_type,
+        birth_place_uezd,
+        birth_place_volost,
+        birth_place,
+        box,
+        doc_type,
+        document_num,
+        documents_pages,
+        event_date_from,
+        first_name,
+        fund_num,
+        inventory_num,
+        last_name,
+        middle_name,
+        vibitie_mesto,
+        ...rest
+      },
+    } = item as ContentItem;
+
+    const notes = Object.fromEntries(
+      Object.entries(rest).filter(
+        ([key]) =>
+          !key.endsWith("_id") &&
+          !key.startsWith("__") &&
+          key !== "id" &&
+          key !== "updated"
+      )
+    );
+
+    return {
+      first_name,
+      last_name,
+      middle_name,
+      birth_date: s(birth_date),
+      birth_place: s([
+        birth_place_gubernia,
+        birth_place_uezd,
+        birth_place_volost,
+        birth_place_np_type,
+        birth_place,
+      ]),
+      record_date: s(event_date_from),
+      record_date_normalized: event_date_from
+        ? new Date(event_date_from)
+        : null,
+      record_place: vibitie_mesto,
+      record_type: doc_type,
+      archive: archive_short,
+      fund: s(fund_num),
+      description: s(inventory_num || box),
+      case: s(document_num),
+      page: s(documents_pages.pages_id),
+      note: s(notes),
+      author_id: "",
+      resource_id: "gwar.mil.ru",
+    };
+  });
